781d216ec478feafc3b7813eaad30edd1c30911b
Kirk Law <kirkperkins99@gmail.com>
2024-07-23 22:47:35 -0700

    Core architecture now completed, now time to create states

--- Assets/StateMachine/StateMachine.cs
+++ Assets/StateMachine/StateMachine.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StateMachine : MonoBehaviour
@@ -9,6 +10,81 @@ public class StateMachine : MonoBehaviour
     StateNode current;
     Dictionary<Type, StateNode> nodes = new();
     HashSet<ITransition> anyTransitions = new();
+
+
+    public void Update()
+    {
+        var transition = GetTransition();
+        if (transition != null) 
+        {
+            ChangeState(transition.To);
+        }
+        current.State?.Update();
+    }
+
+    public void FixedUpdate()
+    {
+        current.State?.FixedUpdate();
+    }
+
+    public void SetState(IState state)
+    {
+        current = nodes[state.GetType()];
+        current.State?.OnEnter();
+    }
+
+    public void ChangeState(IState state)
+    {
+        if (state == current.State) return;
+        var previousState = current.State;
+        var nextState = nodes[state.GetType()].State;
+        previousState?.OnExit();
+        nextState?.OnEnter();
+        current = nodes[state.GetType()];
+    }
+
+    ITransition GetTransition()
+    {
+        foreach (var transition in anyTransitions)
+        {
+            if (transition.Condition.Evaluate())
+            {
+                return transition;
+            }
+        }
+
+        foreach (var transition in current.Transitions)
+        {
+            if (transition.Condition.Evaluate())
+            {
+                return transition;
+            }
+        }
+        return null;
+    }
+
+    public void AddAnyTransition(IState to, IPredicate condition)
+    {
+        anyTransitions.Add(item:new Transition(to, condition));
+    }
+
+    public void AddTransition(IState from, IState to,IPredicate condition)
+    {
+        GetOrAddNode(from).AddTransition(GetOrAddNode(to).State, condition);
+    }
+
+    StateNode GetOrAddNode(IState state)
+    {
+        var node = nodes.GetValueOrDefault(key: state.GetType());
+
+        if (node == null)
+        {
+            node = new StateNode(state);
+            nodes.Add(state.GetType(), node);
+        }
+        return node;
+    }
+
     class StateNode
     {
         public IState State { get; }
