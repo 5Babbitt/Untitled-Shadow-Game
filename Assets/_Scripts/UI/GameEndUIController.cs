93b7e7dea6d0199950ae19502fee51d53b0b6031
Owen Harbert <owen.harbert2@gmail.com>
2024-07-30 01:44:22 +0200

    Add Endgame UI Add Audio Mixer Add Audio Manager

---
+++ Assets/_Scripts/UI/GameEndUIController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// GameEndUIController
+/// </summary>
+public class GameEndUIController : MonoBehaviour
+{
+    public Button menuButton;
+    
+    void Awake()
+    {
+        
+    } 
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
